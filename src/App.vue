d6fca27a937f2637fae4e8fd02bf895aa594232d
iamAndreyGavrilov <79842149+iamAndreyGavrilov@users.noreply.github.com>
2022-04-24 16:50:25 +0500

    using state in Vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,36 @@
 <template>
   <div>Restaurants</div>
+  <button
+    @click="send('FETCH')"
+    :disabled="!(state.matches('ready') || state.matches('initial'))"
+  >
+    Fetch
+  </button>
+
+  <button @click="send('RETRY')" :disabled="!state.matches('failure')">
+    Retry
+  </button>
+
+  <div><span v-show="state.matches('loading')">... Loading ...</span></div>
+
+  <div
+    v-if="
+      state.matches('ready') ||
+      state.matches('loading') ||
+      state.matches('success')
+    "
+  >
+    <div :key="restaurant.id" v-for="restaurant in state.context.restaurants">
+      <h3>{{ restaurant.name }}</h3>
+      <p>Rating - {{ restaurant.rating }} stars</p>
+      <p>{{ restaurant.review }}</p>
+    </div>
+  </div>
+
+  <div v-if="state.matches('failure')">
+    <h4>Error loading restaurants</h4>
+    <p>{{ state.contex.error.message }}</p>
+  </div>
 </template>
 
 <script lang="ts">
@@ -11,8 +42,17 @@ export default defineComponent({
   name: 'App',
   setup() {
     const { state, send } = useMachine(fetchMachine, { devTools: true });
+
+    return {
+      state,
+      send,
+    };
   },
 });
 </script>
 
-<style></style>
+<style>
+button {
+  margin: 10px;
+}
+</style>
